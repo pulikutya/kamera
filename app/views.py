38b77b9fd894dd10eda0676144e4504b400594d3
Márton Balogh <balogh.marton.21f@szlgbp.hu>
2024-06-07 13:39:49 +0200

    f

---
+++ app/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import StreamingHttpResponse
+from django.shortcuts import render
+import cv2
+from threading import Thread
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+        Thread(target=self.update, args=()).start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        image = self.frame
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+    def update(self):
+        while True:
+            (self.grabbed, self.frame) = self.video.read()
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+def video_feed(request):
+    return StreamingHttpResponse(gen(VideoCamera()),
+                                 content_type='multipart/x-mixed-replace; boundary=frame')
+
+def index(request):
+    return render(request, 'stream/index.html')
